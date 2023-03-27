9fae99592f7780ebd4758c767cca1bbeb7c6201a
neta <netami@ac.sce.ac.il>
2023-03-27 14:39:53 +0300

    install chai and mocha for tests

---
+++ Tests/array.spec.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+
+describe('Arrays', () => {
+    describe('#sort', () => {
+        it('Sorting names array', () => {
+            // eslint-disable-next-line no-var
+            var names = ['Dany', 'Moshe', 'Adam'];
+            expect(names.sort()).to.be.eql(['Adam', 'Dany', 'Moshe']);
+        });
+    });
+});
