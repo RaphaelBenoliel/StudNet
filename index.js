425155f9ed4cc688aeec609da9eba3da1a0ea2d7
Sharonpa1 <94108962+Sharonpa1@users.noreply.github.com>
2023-03-27 13:20:00 +0300

    Install nodemon

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT||3000;
 app.get('/', (req, res) => {
-    console.log("New request from HomePage.");
-    res.send("Test");
+    console.log('New request from HomePage.');
+    res.send('Test');
+});
+
+app.listen(port, () => {
+    console.log(`Server is up and running at port: ${port}`);
 });
