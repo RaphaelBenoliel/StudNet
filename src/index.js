fb8bb5e35d4a173dd3fdc837e8e496a80621401d
Raphael Benoliel <raphael2gb@gmail.com>
2023-05-10 16:18:33 +0300

    init mongoDB

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-console */
 import express from 'express';
+import mongoose from 'mongoose';
+// eslint-disable-next-line import/no-unresolved
 import cors from 'cors';
 import authRouter from './routes/AuthRoute.js';
 
 const app = express();
 
+const uri = 'mongodb+srv://raphabr:admin@studnetcluster.zu0mdlt.mongodb.net/?retryWrites=true&w=majority';
+async function connectToDB() {
+  try {
+    mongoose.connect(uri);
+    console.log('Connected to DB');
+  } catch (error) {
+    console.log('Error connecting to DB');
+  }
+}
+connectToDB();
 const configureApp = () => {
   app.use(cors());
 };
@@ -23,9 +35,9 @@ app.get('/', (req, res) => {
 const startServer = async () => {
   configureApp();
   addRouters();
-  const port = process.env.PORT || 4000;
+  const port = process.env.PORT || 1000;
   app.listen(port, () => {
-    console.log(`Server is up and running at port: ${port}`);
+    console.log(`Server is running at port: ${port}`);
   });
 };
 
