1d4bcab57c504b74a97b9619b21413c1b34f5d6b
Raphael Benoliel <raphael2gb@gmail.com>
2023-05-20 03:22:32 +0300

    fix post route & action

--- src/TableActions/PostActions.js
+++ src/TableActions/PostActions.js
@@ -2,12 +2,14 @@ import mongoose from 'mongoose';
 import '../module/PostInfo.js';
 
 const Post = mongoose.model('PostInfo');
+
 export const createPost = async (req) => {
+  console.log('req:', req);
   const post = await Post.create(req);
   return post;
 };
 
 export const getPosts = async (req) => {
-  const posts = await Post.find(req);
+  const posts = await Post.find(req).populate('userID', 'firstName lastName picture');
   return posts;
 };
--- src/routes/PostRoute.js
+++ src/routes/PostRoute.js
@@ -1,20 +1,27 @@
 import express from 'express';
 
-import { createPost } from '../TableActions/PostActions.js';
+import { createPost, getPosts } from '../TableActions/PostActions.js';
 import { respond } from './utils.js';
 
 const postRouter = express.Router();
 postRouter.use(express.json());
 // eslint-disable-next-line consistent-return
 postRouter.post('/cpost', async (req, res) => {
-  const {
-    userID, content,
-  } = req.body;
-  const result = await createPost({
-    userID, content,
-  });
+  const { auth, content } = req.body;
+  const { _id: userID } = JSON.parse(auth);
+  console.log('req:', req.body);
+  const result = await createPost({ userID, content });
+  console.log(result);
   respond(result, res);
   // res.status(result.status).json(result.json);
+});
+
+postRouter.post('/posts', async (req, res) => {
+//   const { auth } = req.body;
+//   const { _id: userID } = JSON.parse(auth);
+//   console.log('userID:', userID);
+  const result = await getPosts();
+  respond(result, res);
 });
 
 export default postRouter;
