2738792429bd23647558eaeb55f23f7dc9513b04
Raphael Benoliel <raphael2gb@gmail.com>
2023-05-21 17:04:28 +0300

    find the correct route

--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
-/* eslint-disable no-underscore-dangle */
-/* eslint-disable no-console */
+/* eslint-disable import/no-named-as-default-member */
 /* eslint-disable import/extensions */
-
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-console */
 import express from 'express';
 import mongoose from 'mongoose';
+// eslint-disable-next-line import/no-unresolved
 import cors from 'cors';
 import authRouter from './routes/AuthRoute.js';
 import postRouter from './routes/PostRoute.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 const app = express();
 
 const uri = 'mongodb+srv://raphabr:admin@studnetcluster.zu0mdlt.mongodb.net/?retryWrites=true&w=majority';
 async function connectToDB() {
   try {
-    await mongoose.connect(uri);
+    mongoose.connect(uri);
     console.log('Connected to DB');
   } catch (error) {
     console.log('Error connecting to DB');
@@ -30,29 +26,23 @@ const configureApp = () => {
 };
 
 const addRouters = () => {
-  app.use('/api/auth', authRouter);
-  app.use('/api/posts', postRouter);
+  app.use('/', authRouter);
+  app.use('/', postRouter);
+  // app.use('/log', authRouter);
 };
+
+app.get('*', (req, res) => {
+  console.log('New request from Backend.');
+  res.send('<h1>Hi from srever<h1/>');
+});
 
 connectToDB();
 const startServer = async () => {
   configureApp();
   addRouters();
-
-  // Serve the React app
-  app.use(express.static(path.join(__dirname, '../client/build')));
-
-  // API routes
-
-  // Catch-all route for serving the React app
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
-
   const port = process.env.PORT || 5002;
   app.listen(port, () => {
     console.log(`Server is running at port: ${port}`);
   });
 };
-
 await startServer();
