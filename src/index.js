a5ead35427dffcc91fc98a3d1e88b294266f6b45
Raphael Benoliel <raphael2gb@gmail.com>
2023-05-21 16:35:33 +0300

    find the correct route

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
 /* eslint-disable no-underscore-dangle */
-/* eslint-disable import/no-named-as-default-member */
-/* eslint-disable import/extensions */
-/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-console */
+/* eslint-disable import/extensions */
+
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import express from 'express';
 import mongoose from 'mongoose';
-import { fileURLToPath } from 'url';
 import cors from 'cors';
-import path from 'path';
 import authRouter from './routes/AuthRoute.js';
 import postRouter from './routes/PostRoute.js';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
 
 const app = express();
 
 const uri = 'mongodb+srv://raphabr:admin@studnetcluster.zu0mdlt.mongodb.net/?retryWrites=true&w=majority';
 async function connectToDB() {
   try {
-    mongoose.connect(uri);
+    await mongoose.connect(uri);
     console.log('Connected to DB');
   } catch (error) {
     console.log('Error connecting to DB');
@@ -31,33 +30,29 @@ const configureApp = () => {
 };
 
 const addRouters = () => {
-  app.use('/', authRouter);
-  app.use('/', postRouter);
-  // app.use('/log', authRouter);
+  app.use('/api/auth', authRouter);
+  app.use('/api/posts', postRouter);
 };
-
-// app.get('*', (req, res) => {
-//   console.log('New request from Backend.');
-//   res.send('<h1>Hi from srever<h1/>');
-// });
 
 connectToDB();
 const startServer = async () => {
   configureApp();
   addRouters();
-  app.use(express.static(path.join(__dirname, 'public/index.html')));
+
+  // Serve the React app
+  app.use(express.static(path.join(__dirname, '../client/build')));
 
   // API routes
-  app.use('/api/auth', authRouter);
-  app.use('/api/posts', postRouter);
 
-  // Serve the React app for all other routes
+  // Catch-all route for serving the React app
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
+
   const port = process.env.PORT || 5002;
   app.listen(port, () => {
     console.log(`Server is running at port: ${port}`);
   });
 };
+
 await startServer();
